563f23cc5d8d22a25835617e5d9843766e94e4da
Chandler Heath <chandler.heath@gmail.com>
2022-02-28 20:13:26 +1300

    Add phase 3 info banner

--- src/component/searchShoutout/searchShoutout.component.tsx
+++ src/component/searchShoutout/searchShoutout.component.tsx
@@ -12,13 +12,22 @@ export class SearchShoutout extends AppComponent {
         <h1 className="searchShoutout__title">{"Locations of Interest in Aotearoa New Zealand"}</h1>
         <p className="searchShoutout__text">{"Places and times where the New Zealand public may have been exposed to COVID-19."}</p>
         <p className="searchShoutout__text">{"If you’ve attended one of these locations during the relevant times please follow the directions for that location."}</p>
-        <p className="searchShoutout__text">{"Low-risk locations of interest will not always be published for regions that are Red under the COVID-19 Protection Framework. High-risk locations of interest are published for all regions."}</p>
         <p className="searchShoutout__text">
           {"Data provided by the "}
           <a href="https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest" target="_blank" rel="noopener" className="searchShoutout__text__link">
             {"Ministry of Health"}
           </a>
           {". The data source is expected to update hourly. Locations are removed after 14 days."}</p>
+        <div className="searchShoutout__banner">
+          <h2 className="searchShoutout__title">{"Locations of Interest During Omicron Phase 3"}</h2>
+          <p className="searchShoutout__text">{"The Ministry of Health is not currently publishing new locations of interest. Case investigations are instead focused on high-risk locations and household contacts."}</p>
+          <p className="searchShoutout__text">
+          {"For more information, please visit the "}
+          <a href="https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest" target="_blank" rel="noopener" className="searchShoutout__text__link">
+            {"Ministry of Health"}
+          </a>
+          {" website."}</p>
+        </div>
       </section>
     );
   }
